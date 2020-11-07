b02b5a95ba9d50e719c925b0ed5e01c6e86ed991
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2020-11-07 22:41:55 +0700

    Fix layout setup with Fragment

--- app/src/main/java/com/excode/telkomindonesia/MainActivity.java
+++ app/src/main/java/com/excode/telkomindonesia/MainActivity.java
@@ -1,17 +1,46 @@
 package com.excode.telkomindonesia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import javax.net.ssl.HostnameVerifier;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+        switch (item.getItemId()) {
+            case R.id.home_menu:
+                selectedFragment = new HomeFragment();
+                break;
+            case R.id.place_menu:
+                selectedFragment = new PlaceFragment();
+                break;
+            case R.id.contact_menu:
+                selectedFragment = new ContactFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        return true;
+    }
+
 }
