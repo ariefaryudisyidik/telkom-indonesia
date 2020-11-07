e90a5dfd53e1c1e877812acdae17ddc588f23230
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2020-11-08 00:24:37 +0700

    Testing maps

--- app/src/main/java/com/excode/telkomindonesia/MainActivity.java
+++ app/src/main/java/com/excode/telkomindonesia/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -33,6 +35,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 break;
             case R.id.place_menu:
                 selectedFragment = new PlaceFragment();
+                Intent mapsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/place/Telkom+Indonesia+Cabang+Kedaton+-+Unit/@-5.3781389,105.2600335,17z/data=!4m13!1m7!3m6!1s0x0:0x0!2zNcKwMjInNDEuMyJTIDEwNcKwMTUnNDQuMCJF!3b1!8m2!3d-5.3781389!4d105.2622222!3m4!1s0x2e40dad62159b2e3:0xb0f8a18037de1eff!8m2!3d-5.3781636!4d105.2622125"));
+                startActivity(mapsIntent);
                 break;
             case R.id.contact_menu:
                 selectedFragment = new ContactFragment();
