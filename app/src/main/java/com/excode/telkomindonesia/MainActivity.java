548d9101f2de4f5e19aa484bee280a2f6068dfbd
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2020-11-07 22:09:30 +0700

    Add Place Activity

--- app/src/main/java/com/excode/telkomindonesia/MainActivity.java
+++ app/src/main/java/com/excode/telkomindonesia/MainActivity.java
@@ -1,17 +1,36 @@
 package com.excode.telkomindonesia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home_menu);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.place_menu:
+                        startActivity(new Intent(getApplicationContext(), PlaceActivity.class));
+                        overridePendingTransition(0, 0);
+                }
+                return false;
+            }
+        });
     }
 }
