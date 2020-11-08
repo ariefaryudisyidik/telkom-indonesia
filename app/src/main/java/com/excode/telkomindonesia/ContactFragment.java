a805b093e3e5c5a90faf54d384ccb57961c001a2
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2020-11-08 13:25:09 +0700

    Add cardview in fragment_contact.xml

--- app/src/main/java/com/excode/telkomindonesia/ContactFragment.java
+++ app/src/main/java/com/excode/telkomindonesia/ContactFragment.java
@@ -1,5 +1,6 @@
 package com.excode.telkomindonesia;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +16,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ContactFragment extends Fragment {
+    Button btnWeb, btnEmail, btnTelephone;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
